ccc771689a14856e08608d2a352086cb9ebf3a74
liaowei <liaowei@cestc.cn>
2021-05-26 10:44:21 +0800

    1.删除agenda；2.清理代码

--- src/web/js/main.js
+++ src/web/js/main.js
@@ -1,45 +1,41 @@
-import routers from './routers.js';
+const routers = [
+    {
+        name: 'cpi',
+        desc: 'cpi',
+        url: './cpi.html',
+    },
+    {
+        name: 'moneySupply',
+        desc: 'moneySupply',
+        url: './moneySupply.html',
+    },
+    {
+        name: 'ticketPutAndBackStatByMonth',
+        desc: 'ticketPutAndBackStatByMonth',
+        url: './ticketPutAndBackStatByMonth.html',
+    }
+];
 
 function renderTable() {
-    let tableElem = document.querySelector('#list');
-    if (!tableElem) {
-        tableElem = document.createElement('table');
-        tableElem.id = 'list';
-        const thead = `
-            <thead>
-                <tr>
-                    <th>名称</th>
-                    <th>描述</th>
-                    <th>操作</th>
-                </tr>
-            </thead>
-        `;
-        const tbody = routers.map(d => `
+    const tableElem = document.createElement('table');
+    const thead = `
+        <thead>
             <tr>
-                <td>${d.name}</td>
-                <td>${d.desc}</td>
-                <td><a href="#${d.name}">查看</a></td>
+                <th>名称</th>
+                <th>描述</th>
+                <th>操作</th>
             </tr>
-        `).join('');
-        tableElem.innerHTML = thead + tbody;
-        document.body.appendChild(tableElem);
-    } else {
-        tableElem.style.display = 'unset';
-    }
+        </thead>
+    `;
+    const tbody = routers.map(d => `
+        <tr>
+            <td>${d.name}</td>
+            <td>${d.desc}</td>
+            <td><a href="${d.url}">查看</a></td>
+        </tr>
+    `).join('');
+    tableElem.innerHTML = thead + tbody;
+    return tableElem;
 }
 
-function handleHashChange() {
-    const hash = location.hash.substr(1);
-    const router = routers.find(d => d.name === hash);
-    if (router) {
-        const tableElem = document.querySelector('#list');
-        if (tableElem) tableElem.style.display = 'none';
-        import(router.script).then(() => console.log(`加载`, router));
-    } else {
-        renderTable();
-    }
-}
-
-window.addEventListener('hashchange', handleHashChange);
-
-handleHashChange();
+document.body.appendChild(renderTable());
