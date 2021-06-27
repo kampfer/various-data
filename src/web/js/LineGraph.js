46dfaf1ee8bea14c6666c8d5dfb514cad74313fe
liaowei <mr_liaowei@163.com>
2021-06-27 11:59:44 +0800

    轴线颜色、轴标签颜色、刻度线颜色可配置

--- src/web/js/LineGraph.js
+++ src/web/js/LineGraph.js
@@ -1,39 +1,89 @@
+function mergeSettings(target, source) {
+    if (source) {
+        for(let attr in target) {
+            if (source[attr]) target[attr] = source[attr];
+        }
+    }
+}
+
 class LineGraph {
 
     constructor({
-        width = 300,
-        height = 150,
+        chart,
         container,
-        margin = { top: 0, right: 0, bottom: 0, left: 0 }
+        xAxis,
+        yAxis
     }) {
-        this.width = width;
-        this.height = height;
-        this.margin = margin;
-        this.svgSelection = d3.create('svg').attr('viewBox', [0, 0, width, height]);
-        this.xAxisSelection = this.svgSelection.append('g');
-        this.yAxisSelection = this.svgSelection.append('g');
-        this.seriesWrapperSelection = this.svgSelection.append('g');
 
-        if (container) {
-            if (container instanceof HTMLElement) {
-                container.appendChild(this.svgSelection.node());
-            } else {
-                document.querySelector(container).appendChild(this.svgSelection.node());
-            }
+        if (typeof container === 'string') {
+            container = document.querySelector(container);
         }
+
+        let containerWidth, containerHeight;
+        if (container instanceof HTMLElement) {
+            containerWidth = container.offsetWidth;
+            containerHeight = container.offsetHeight;
+        }
+
+        // https://api.highcharts.com.cn/highcharts#chart
+        this._chart = {
+            width: containerWidth || 300,
+            height: containerHeight || 150,
+            spacing: [0, 0, 0, 0]
+        };
+
+        // https://api.highcharts.com.cn/highcharts#xAxis
+        this._xAxis = {
+            lineColor: '#ccd6eb',
+            tickColor: '#ccd6eb',
+            labels: {
+                style: {
+                    color: '#666',
+                }
+            }
+        };
+
+        // https://api.highcharts.com.cn/highcharts#yAxis
+        this._yAxis = {
+            lineColor: '#ccd6eb',
+            tickColor: '#ccd6eb',
+            labels: {
+                style: {
+                    color: '#666',
+                }
+            }
+        };
+
+        mergeSettings(this._chart, chart);
+        mergeSettings(this._xAxis, xAxis);
+        mergeSettings(this._yAxis, yAxis);
+
+        this._svgSelection = d3.create('svg')
+            .attr('viewBox', [0, 0, this._chart.width, this._chart.height])
+            .attr('width', this._chart.width)
+            .attr('height', this._chart.height);
+
+        if (container instanceof HTMLElement) {
+            container.appendChild(this._svgSelection.node());
+        }
+
+    }
+
+    node() {
+        return this._svgSelection.node();
     }
 
     setData(data) {
-        const width = this.width;
-        const height = this.height;
-        const margin = this.margin;
+        const width = this._chart.width;
+        const height = this._chart.height;
+        const spacing = this._chart.spacing;
         const { labels, series } = data;
 
         this.data = data;
 
         this.xScale = d3.scaleUtc()
             .domain(d3.extent(labels))
-            .range([margin.left, width - margin.left]);
+            .range([spacing[3], width - spacing[3]]);
         
         this.yScale = d3.scaleLinear()
             .domain([
@@ -41,7 +91,7 @@ class LineGraph {
                 d3.max(series, d => d3.max(d.data))
             ])
             .nice()
-            .range([height - margin.bottom, margin.top]);
+            .range([height - spacing[2], spacing[0]]);
 
         this.line = d3.line()
             .defined(d => !isNaN(d) && d !== null)
@@ -50,27 +100,47 @@ class LineGraph {
     }
 
     render() {
-        const width = this.width;
-        const height = this.height;
-        const margin = this.margin;
+        const width = this._chart.width;
+        const height = this._chart.height;
+        const spacing = this._chart.spacing;
         const xScale = this.xScale;
         const yScale = this.yScale;
 
-        this.xAxisSelection
-            .attr('transform', `translate(0, ${height - margin.bottom})`)
+        const [
+            xAxisWrapperSelection,
+            yAxisWrapperSelection,
+            seriesWrapperSelection
+        ] = ['xAxisWrapper', 'yAxisWrapper', 'seriesWrapper'].map(selector => {
+            let selection = this._svgSelection.selectAll(`.${selector}`);
+            if (selection.empty()) {
+                selection = this._svgSelection.append('g').classed(selector, true);
+            }
+            return selection;
+        });
+
+        xAxisWrapperSelection
+            .attr('transform', `translate(0, ${height - spacing[2]})`)
             .call(
                 d3.axisBottom(xScale)
                   .ticks(width / 80)
                   .tickSizeOuter(0)
             )
-            .call(g => g.selectAll('.tick text').attr('fill', '#666'));
+            .call(g => {
+                g.selectAll('.domain').attr('stroke', this._xAxis.lineColor);
+                g.selectAll('.tick text').attr('fill', this._xAxis.labels.style.color);
+                g.selectAll('.tick line').attr('stroke', this._xAxis.tickColor);
+            });
 
-        this.yAxisSelection
-            .attr('transform', `translate(${margin.left}, 0)`)
+        yAxisWrapperSelection
+            .attr('transform', `translate(${spacing[3]}, 0)`)
             .call(d3.axisLeft(yScale))
-            .call(g => g.selectAll('.tick text').attr('fill', '#666'));
+            .call(g => {
+                g.selectAll('.domain').attr('stroke', this._yAxis.lineColor);
+                g.selectAll('.tick text').attr('fill', this._yAxis.labels.style.color);
+                g.selectAll('.tick line').attr('stroke', this._yAxis.tickColor);
+            });
 
-        this.seriesWrapperSelection
+        seriesWrapperSelection
             .selectAll('g')
             .data(this.data.series)
             .join('g')
