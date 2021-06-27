46cf730384d27aa4777f0b2097b4ea38f3d76d47
liaowei <mr_liaowei@163.com>
2021-06-27 15:22:10 +0800

    增加title和subtitle

--- src/web/js/LineGraph.js
+++ src/web/js/LineGraph.js
@@ -4,7 +4,14 @@ function mergeSettings(target, source) {
             if (source[attr]) target[attr] = source[attr];
         }
     }
+    return target;
 }
+
+const textAlignToTextAnchor = {
+    left: 'start',
+    center: 'middle',
+    right: 'end'
+};
 
 class LineGraph {
 
@@ -12,7 +19,9 @@ class LineGraph {
         chart,
         container,
         xAxis,
-        yAxis
+        yAxis,
+        title,
+        subtitle,
     }) {
 
         if (typeof container === 'string') {
@@ -26,14 +35,14 @@ class LineGraph {
         }
 
         // https://api.highcharts.com.cn/highcharts#chart
-        this._chart = {
+        this._chart = mergeSettings({
             width: containerWidth || 300,
             height: containerHeight || 150,
             spacing: [0, 0, 0, 0]
-        };
+        }, chart);
 
         // https://api.highcharts.com.cn/highcharts#xAxis
-        this._xAxis = {
+        this._xAxis = mergeSettings({
             lineColor: '#ccd6eb',
             tickColor: '#ccd6eb',
             labels: {
@@ -41,10 +50,10 @@ class LineGraph {
                     color: '#666',
                 }
             }
-        };
+        }, xAxis);
 
         // https://api.highcharts.com.cn/highcharts#yAxis
-        this._yAxis = {
+        this._yAxis = mergeSettings({
             lineColor: '#ccd6eb',
             tickColor: '#ccd6eb',
             labels: {
@@ -52,11 +61,33 @@ class LineGraph {
                     color: '#666',
                 }
             }
-        };
+        }, yAxis);
 
-        mergeSettings(this._chart, chart);
-        mergeSettings(this._xAxis, xAxis);
-        mergeSettings(this._yAxis, yAxis);
+        // https://api.highcharts.com.cn/highcharts#title
+        this._title = mergeSettings({
+            text: null,
+            align: 'center',
+            style: {
+                color: '#333',
+                fontSize: '18px',
+            }
+        }, title);
+
+        // https://api.highcharts.com.cn/highcharts#subtitle
+        this._subtitle = mergeSettings({
+            text: null,
+            align: 'center',
+            style: {
+                color: '#666',
+                fontSize: '12px',
+            }
+        }, subtitle);
+
+        this._title.x = this._chart.width / 2;
+        this._title.y = parseFloat(this._title.style.fontSize, 10) * (4 / 3);
+
+        this._subtitle.x = this._title.x;
+        this._subtitle.y = this._title.y + 10 + parseFloat(this._subtitle.style.fontSize, 10);
 
         this._svgSelection = d3.create('svg')
             .attr('viewBox', [0, 0, this._chart.width, this._chart.height])
@@ -79,7 +110,11 @@ class LineGraph {
         const spacing = this._chart.spacing;
         const { labels, series } = data;
 
-        this.data = data;
+        series.forEach((item, i) => {
+            item.color = item.color || d3.schemeCategory10[i % 10];
+        });
+
+        this._data = data;
 
         this.xScale = d3.scaleUtc()
             .domain(d3.extent(labels))
@@ -109,11 +144,28 @@ class LineGraph {
         const [
             xAxisWrapperSelection,
             yAxisWrapperSelection,
-            seriesWrapperSelection
-        ] = ['xAxisWrapper', 'yAxisWrapper', 'seriesWrapper'].map(selector => {
+            seriesWrapperSelection,
+            titleSelection,
+            subtitleSelection
+        ] = [{
+            selector: 'xAxisWrapper',
+            tag: 'g',
+        }, {
+            selector: 'yAxisWrapper',
+            tag: 'g',
+        }, {
+            selector: 'seriesWrapper',
+            tag: 'g',
+        }, {
+            selector: 'title',
+            tag: 'text',
+        }, {
+            selector: 'subtitle',
+            tag: 'text',
+        }].map(({ selector, tag }) => {
             let selection = this._svgSelection.selectAll(`.${selector}`);
             if (selection.empty()) {
-                selection = this._svgSelection.append('g').classed(selector, true);
+                selection = this._svgSelection.append(tag).classed(selector, true);
             }
             return selection;
         });
@@ -142,7 +194,7 @@ class LineGraph {
 
         seriesWrapperSelection
             .selectAll('g')
-            .data(this.data.series)
+            .data(this._data.series)
             .join('g')
             .append('path')
                 .attr('fill', 'none')
@@ -151,6 +203,23 @@ class LineGraph {
                 .attr('stroke-linecap', 'round')
                 .attr('stroke', d => d.color || '#000')
                 .attr('d', d => this.line(d.data));
+
+        titleSelection
+            .text(this._title.text)
+            .attr('fill', this._title.style.color)
+            .attr('font-size', this._title.style.fontSize)
+            .attr('text-anchor', textAlignToTextAnchor[this._title.align])
+            .attr('x', this._title.x)
+            .attr('y', this._title.y);
+
+        subtitleSelection
+            .text(this._subtitle.text)
+            .attr('fill', this._subtitle.style.color)
+            .attr('font-size', this._subtitle.style.fontSize)
+            .attr('text-anchor', textAlignToTextAnchor[this._subtitle.align])
+            .attr('x', this._subtitle.x)
+            .attr('y', this._subtitle.y);
+        
     }
 
 }
