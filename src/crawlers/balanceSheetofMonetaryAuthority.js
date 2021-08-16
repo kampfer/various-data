1ab8593dfaf72ad680addf10e09260ffc91a809b
liaowei <l.w.kampfer@gmail.com>
2021-08-16 16:35:39 +0800

    解决央行资产负债表数据缺失的问题

--- src/crawlers/balanceSheetofMonetaryAuthority.js
+++ src/crawlers/balanceSheetofMonetaryAuthority.js
@@ -4,15 +4,42 @@ import path from 'path';
 import { ROOT_PATH } from '../constants.js';
 
 const sources = [
-    'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2021/07/2021071516002091976.htm',   // 2021
-    'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2021/01/2021011817520862940.htm',   // 2020
-    'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2020/01/2020011716363947588.htm',   // 2019
-    'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2019/01/2019011618592270314.htm',   // 2018
-    'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2018/01/2018011515315872258.htm',   // 2017
-    'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2017/01/2017011616170598114.htm',   // 2016
-    'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2016/01/2016011510420280670.htm',   // 2015
-    'http://www.pbc.gov.cn/eportal/fileDir/defaultCurSite/resource/cms/2015/07/2014s04.htm',    // 2014
-    'http://www.pbc.gov.cn/eportal/fileDir/defaultCurSite/resource/cms/2015/07/2013s04.htm',    // 2013
+    {
+        url: 'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2021/07/2021071516002091976.htm',  // 2021
+        rows: [4, 29],
+    },
+    {
+        url: 'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2021/01/2021011817520862940.htm',   // 2020
+        rows: [4, 29]
+    },
+    {
+        url: 'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2020/01/2020011716363947588.htm',   // 2019
+        rows: [4, 29]
+    },
+    {
+        url: 'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2019/01/2019011618592270314.htm',   // 2018
+        rows: [4, 27]
+    },
+    {
+        url: 'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2018/01/2018011515315872258.htm',   // 2017
+        rows: [4, 27]
+    },
+    {
+        url: 'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2017/01/2017011616170598114.htm',   // 2016
+        rows: [4, 26]
+    },
+    {
+        url: 'http://www.pbc.gov.cn/diaochatongjisi/resource/cms/2016/01/2016011510420280670.htm',   // 2015
+        rows: [4, 26]
+    },
+    {
+        url: 'http://www.pbc.gov.cn/eportal/fileDir/defaultCurSite/resource/cms/2015/07/2014s04.htm',    // 2014
+        rows: [4, 26]
+    },
+    {
+        url: 'http://www.pbc.gov.cn/eportal/fileDir/defaultCurSite/resource/cms/2015/07/2013s04.htm',    // 2013
+        rows: [4, 26]
+    },
 ];
 
 export default async function balanceSheetofMonetaryAuthority() {
@@ -23,14 +50,14 @@ export default async function balanceSheetofMonetaryAuthority() {
         .setChromeOptions(opts.headless())
         .build();
     let data = [];
-    for(let source of sources) {
-        await driver.get(source);
+    for(let { url, rows } of sources) {
+        await driver.get(url);
         const body = await driver.findElement(By.css('table'));
         const content = await body.getText();
-        // console.log(content.split('\n').slice(4, -4));
+        // console.log(content.split('\n').slice(...rows));
         const regx = /^([\u4e00-\u9fa5\s：]+)([A-Za-z\s-: ]+)([\d.\s]+)/;
         const yearData = [];
-        content.split('\n').slice(4, -4).map((str, i) => {
+        content.split('\n').slice(...rows).map((str, i) => {
             // console.log(str.match(regx));
             const matches = str.match(regx);
             if (i === 0) {
@@ -54,3 +81,5 @@ export default async function balanceSheetofMonetaryAuthority() {
         data
     };
 }
+
+balanceSheetofMonetaryAuthority();
