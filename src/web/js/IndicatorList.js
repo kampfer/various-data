39cdd2a5af8da6a3bea9156269f8ef8451592774
liaowei <mr_liaowei@163.com>
2021-08-16 22:44:35 +0800

    update

--- src/web/js/IndicatorList.js
+++ src/web/js/IndicatorList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {
     Table,
-    Space
+    Space,
+    Spin,
+    message
 } from 'antd';
 import indicators from './indicators.js';
 import {
@@ -9,49 +11,59 @@ import {
 } from "react-router-dom";
   
 import 'antd/dist/antd.css';
-
-const updateIndicator = (name) => {
-    if (name) {
-        fetch(`/api/update?name=${name}`)
-            .then(res => res.json())
-            .then(json => {
-                if (json.code === 200) alert('更新成功！');
-            });
-    }
-};
-
-const columns = [
-    {
-        title: '名称',
-        key: 'name',
-        dataIndex: 'name',
-    },
-    {
-        title: '描述',
-        key: 'desc',
-        dataIndex: 'desc',
-    },
-    {
-        title: '操作',
-        key: 'action',
-        render: (text, record, /*index*/) => (
-            <Space size='middle'>
-                <Link to={`/indicator/${record.name}`}>查看</Link>
-                <a onClick={() => updateIndicator(record.name)}>更新</a>
-            </Space>
-        )
-    }
-];
+import './IndicatorList.css';
 
 export default class IndicatorList extends React.Component {
 
-    // constructor(props) {
-    //     super(props);
-    // }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            columns: [
+                {
+                    title: '名称',
+                    key: 'name',
+                    dataIndex: 'name',
+                },
+                {
+                    title: '描述',
+                    key: 'desc',
+                    dataIndex: 'desc',
+                },
+                {
+                    title: '操作',
+                    key: 'action',
+                    render: (text, record, /*index*/) => (
+                        <Space size='middle'>
+                            <Link to={`/indicator/${record.name}`}>查看</Link>
+                            <a onClick={() => this.updateIndicator(record.name)}>更新</a>
+                        </Space>
+                    )
+                }
+            ],
+            loading: false,
+        };
+
+        this.updateIndicator = this.updateIndicator.bind(this);
+    }
+
+    updateIndicator(name) {
+        if (name) {
+            this.setState({loading: true});
+            fetch(`/api/update?name=${name}`)
+                .then(res => res.json())
+                .then(json => {
+                    if (json.code === 200) message.success(`${name}更新成功`);
+                })
+                .finally(() => this.setState({ loading: false }));
+        }
+    }
 
     render() {
         return (
-            <Table dataSource={indicators} columns={columns} pagination={{hideOnSinglePage: true}} rowKey='name'/>
+            <Spin spinning={this.state.loading}>
+                <Table dataSource={indicators} columns={this.state.columns} pagination={{hideOnSinglePage: true}} rowKey='name'/>
+            </Spin>
         );
     }
 
