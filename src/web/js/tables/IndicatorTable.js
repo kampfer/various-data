c492d8e64d6371fdd6e262249c2fce45ec6364ff
liaowei <mr_liaowei@163.com>
2021-09-06 22:59:06 +0800

    实现拥有动态列的表格

--- src/web/js/tables/IndicatorTable.js
+++ src/web/js/tables/IndicatorTable.js
@@ -1,19 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     useParams,
 } from 'react-router-dom';
-import { Table } from 'antd';
+import { Table, Button } from 'antd';
 
 export default function IndicatorTable() {
 
     const { name } = useParams();
-
-    const columns = [];
+    const [columns, setColumns] = useState([]);
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        const { data } = fetch('data/ticketPutAndBackStatByMonth.json').then(response => response.json());
-    });
+        fetch(`data/${name}.json`)
+            .then(response => response.json())
+            .then(({ data }) => {
+                setColumns([
+                    ...Object.keys(data[0]).map((d) => ({ title: d, dataIndex: d })),
+                    {
+                        title: '操作',
+                        dataIndex: 'operation',
+                        render: () => 1
+                    }
+                ]);
+                setData(data);
+            });
 
-    return (<Table></Table>);
+    }, [name]);
+
+    const addRow = () => {};
+
+    return (
+        <div style={{ padding: 10 }}>
+            <Button
+                onClick={addRow}
+                type='primary'
+                style={{
+                    margin: '16px 0',
+                }}
+            >
+                Add a row
+            </Button>
+            <Table dataSource={data} columns={columns} bordered></Table>
+        </div>
+    );
 
 }
