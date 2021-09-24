8f197c24c5ed1d3e3186f7e381222f23c69ab791
liaowei <liaowei@cestc.cn>
2021-09-24 09:32:09 +0800

    data

--- src/web/js/graphs/index.js
+++ src/web/js/graphs/index.js
@@ -4,4 +4,3 @@ export { default as ticketPutAndBackStatByMonth } from './TicketGraph.js';
 export { default as fr_fdr } from './FrFdrGraph.js';
 export { default as CFETSRMBIndex } from './CFETSRMBIndexGraph.js';
 export { default as balanceSheetOfMonetaryAuthority } from './BalanceSheetOfMonetaryAuthorityGraph.js';
-export { default as a } from './a.js';
