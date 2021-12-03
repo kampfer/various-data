640907c74be01d683f9a3b79c835a52a0c0ae39e
liaowei <liaowei@cestc.cn>
2021-12-03 17:43:51 +0800

    update

--- src/web/js/App.js
+++ src/web/js/App.js
@@ -9,24 +9,88 @@ import IndicatorList from './IndicatorList.js';
 import IndicatorGraphRouter from './IndicatorGraphRouter.js';
 import IndicatorTableRouter from './IndicatorTableRouter.js';
 
+// function AppState(app) {
+//     const indicatorList = [];
+//     return {
+//         indicatorList,
+//         addIndicator(values) {
+//             return fetch(`/api/addIndicator`, {
+//                 body: JSON.stringify(values),
+//                 method: 'POST',
+//                 headers: {
+//                     'content-type': 'application/json'
+//                 }
+//             })
+//             .then(res => res.json())
+//             .then(({ code, msg, data }) => {
+//                 if (code === 200) {
+//                     app.setState({ indicatorList: [...indicatorList, data] });
+//                     return data;
+//                 }
+//             });
+//         },
+//         deleteIndicator() {},
+//         updateIndicator() {},
+//         crawlIndicator() {}
+//     };
+// }
+
+class AppState {
+
+    constructor(app) {
+        this.app = app;
+        this.indicatorList = [];
+    }
+
+    getIndicatorList() {}
+
+    addIndicator(values) {
+        return fetch(`/api/addIndicator`, {
+            body: JSON.stringify(values),
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(({ code, msg, data }) => {
+            if (code === 200) {
+                const newIndicatorList = [...this.indicatorList, data];
+                this.indicatorList = newIndicatorList;
+                this.app.setState({ indicatorList: newIndicatorList });
+                return data;
+            } else {
+                return Promise.reject(msg);
+            }
+        });
+    }
+
+    deleteIndicator() {}
+
+    updateIndicator() {}
+
+    crawlIndicator() {}
+
+}
+
 export default class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = { indicatorList: [] };
+        this.state = new AppState(this);
     }
 
-    addIndicator(data) {
-        const { indicatorList } = this.state;
-        this.setState({ indicatorList: [...indicatorList, data] });
-    }
+    // addIndicator(data) {
+    //     const { indicatorList } = this.state;
+    //     this.setState({ indicatorList: [...indicatorList, data] });
+    // }
 
-    deleteIndicator(id) {
-        const { indicatorList } = this.state;
-        const index = indicatorList.findIndex(d => d.id === id);
-        indicatorList.splice(index, 1);
-        this.setState({ indicatorList: [...indicatorList] });
-    }
+    // deleteIndicator(id) {
+    //     const { indicatorList } = this.state;
+    //     const index = indicatorList.findIndex(d => d.id === id);
+    //     indicatorList.splice(index, 1);
+    //     this.setState({ indicatorList: [...indicatorList] });
+    // }
 
     componentDidMount() {
         fetch(`/api/getIndicatorList`)
@@ -37,7 +101,6 @@ export default class App extends React.Component {
     }
 
     render() {
-        const { indicatorList } = this.state;
         return (
             <Router>
                 <Switch>
@@ -45,13 +108,13 @@ export default class App extends React.Component {
                         <Redirect to='/indicators' />
                     </Route>
                     <Route path='/indicators'>
-                        <IndicatorList indicatorList={indicatorList} onAddIndicator={(data) => this.addIndicator(data)} onDeleteIndicator={(id) => this.deleteIndicator(id)} />
+                        <IndicatorList indicatorList={this.state} />
                     </Route>
                     <Route path='/indicator/table'>
-                        <IndicatorTableRouter indicatorList={indicatorList} />
+                        <IndicatorTableRouter indicatorList={this.state} />
                     </Route>
                     <Route path='/indicator/graph/:id'>
-                        <IndicatorGraphRouter indicatorList={indicatorList} />
+                        <IndicatorGraphRouter indicatorList={this.state} />
                     </Route>
                 </Switch>
             </Router>
