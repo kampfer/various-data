bb45f58502c6b87fc140ef837fcd737350fd907f
liaowei <liaowei@vcg.vom>
2023-08-08 06:09:56 +0800

    抓取civitai图片

--- src/crawlers/nodejs/civitai.js
+++ src/crawlers/nodejs/civitai.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { DATA_PATH } from './constants.js';
 import { existsSync, readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
+import { join, extname } from 'path';
 
-async function main() {
+async function loopImageList() {
   const dataFile = join(DATA_PATH, 'civitai_images.json');
   let url = `https://civitai.com/api/v1/images?period=AllTime&sort=Most Reactions`;
   // let url = 'https://civitai.com/api/v1/images?cursor=9453&period=AllTime&sort=Most%20Reactions';
@@ -29,6 +29,31 @@ async function main() {
     }
     url = metadata.nextPage;
   }
+}
+
+async function _loop(idx, list) {
+  const item = list[idx];
+  const url = item.url;
+  const imageExtname = extname(url);
+  console.log(`抓取: ${item.id} - ${idx} - ${url}`);
+  const res = await axios.get(url, { responseType: 'arraybuffer' });
+  writeFileSync(join(DATA_PATH, 'civitai', `${item.id}${imageExtname}`), res.data);
+  _loop(idx + 1, list);
+}
+
+async function loopImages() {
+  const dataFile = join(DATA_PATH, 'civitai_images.json');
+  if (existsSync(dataFile)) {
+    const listContent = readFileSync(dataFile);
+    const list = JSON.parse(listContent);
+    const start = 22;
+    _loop(start, list);
+  }
+}
+
+async function main() {
+  // loopImageList();
+  loopImages();
 }
 
 main()
