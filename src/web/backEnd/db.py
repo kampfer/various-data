b093446c09ec63529b9b7a12764d9521a6f34903
liaowei <liaowei@vcg.vom>
2023-09-28 17:40:52 +0800

    update sql

--- src/web/backEnd/db.py
+++ src/web/backEnd/db.py
@@ -7,11 +7,11 @@ def generateConditionSql(keyword, startTime, endTime, category, significance):
     print(keyword, startTime, endTime, category, significance)
     condition = []
     if keyword != None:
-        condition.append("content LIKE :keyword")
+        condition.append("n.content LIKE :keyword")
     if startTime != None and endTime != None:
         condition.append("create_time >= :startTime AND create_time <= :endTime")
     if category != None and category != 0:
-        condition.append("tags LIKE :category")
+        condition.append("n.id IN (SELECT news_id from news_tag nt WHERE nt.tag_id = :category)")
     if significance != None and significance != 0:
         condition.append("significance=:significance")
     conditionStr = f"WHERE {' AND '.join(condition)}" if len(condition) > 0 else ""
@@ -26,26 +26,35 @@ class SinaNews7x24DB:
 
     def createTable(self):
         sql = """
-    CREATE TABLE IF NOT EXISTS sina_news_7x24 (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      sina_id INTEGER,
-      create_time INTEGER,
-      content TEXT,
-      url TEXT,
-      tags TEXT,
-      significance INTEGER
-    );
-    """
+        CREATE TABLE IF NOT EXISTS sina_news_7x24 (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            sina_id INTEGER,
+            create_time INTEGER,
+            content TEXT,
+            url TEXT,
+            significance INTEGER
+        );
+        """
         self.conn.execute(sql)
+
         sql = """
-    CREATE TABLE IF NOT EXISTS tag (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT,
-      is_sina_tag BOOLEAN,
-      sina_id TEXT
-    )
-    """
+        CREATE TABLE IF NOT EXISTS tag (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            is_sina_tag BOOLEAN,
+            sina_id TEXT
+        )
+        """
         self.conn.execute(sql)
+
+        sql = """
+        CREATE TABLE IF NOT EXISTS news_tag (
+            news_id INTEGER,
+            tag_id INTEGER
+        )
+        """
+        self.conn.execute(sql)
+
         self.conn.commit()
 
     def recoverFromJson(self, jsonFile):
@@ -54,13 +63,8 @@ class SinaNews7x24DB:
 
         f = open(jsonFile, "r")
         data = json.load(f)
-        rows = []
-        for item in data:
-            tags = []
-            for d in item["tag"]:
-                tagId = self.insertTag(d["name"], d["id"], True)
-                tags.append(tagId)
-
+        total = len(data)
+        for idx, item in enumerate(data):
             # 新闻
             sina_id = item["id"]
             create_time = int(
@@ -69,11 +73,29 @@ class SinaNews7x24DB:
             )
             content = item["rich_text"]
             url = item["docurl"]
-            newsTags = ",".join(map(str, tags))
             significance = 0
-            rows.append((sina_id, create_time, content, url, newsTags, significance))
+            newsId = self.insertNews(sina_id, create_time, content, url, significance)
 
-        self.insertMultipleNews(rows)
+            for d in item["tag"]:
+                tagId = self.insertTag(d["name"], d["id"], True)
+                self.insertRelation(newsId, tagId)
+
+            print(
+                f"insert news: {sina_id} {idx + 1}/{total} {((idx + 1) / total)*100}%"
+            )
+
+    def insertNews(self, sina_id, create_time, content, url, significance):
+        conn = self.conn
+        cursor = conn.cursor()
+        sql = """
+        INSERT INTO sina_news_7x24
+        (sina_id, create_time, content, url, significance)
+        VALUES(?, ?, ?, ?, ?)
+        """
+        cursor.execute(sql, (sina_id, create_time, content, url, significance))
+        conn.commit()
+        print(f"insert news {sina_id}")
+        return cursor.lastrowid
 
     # 创建tag并返回id
     def insertTag(self, name, sinaId, isSinaTag=False):
@@ -85,21 +107,42 @@ class SinaNews7x24DB:
 
         if result is None:
             sql = """
-        INSERT OR IGNORE INTO tag (name, is_sina_tag, sina_id) VALUES (?,?,?)
-      """
-            cursor.execute(sql, (name, sinaId, isSinaTag))
+            INSERT OR IGNORE INTO tag (name, is_sina_tag, sina_id) VALUES (?,?,?)
+            """
+            cursor.execute(sql, (name, isSinaTag, sinaId))
             conn.commit()
             return cursor.lastrowid
         else:
             return result[0]
 
-    def insertMultipleNews(self, list):
+    def insertRelation(self, newsId, tagId):
         sql = """
-      INSERT INTO sina_news_7x24
-      (sina_id, create_time, content, url, tags, significance)
-      VALUES(?, ?, ?, ?, ?, ?)
-    """
+        INSERT OR IGNORE INTO news_tag (news_id, tag_id) VALUES (?,?)
+        """
+        self.conn.execute(sql, (newsId, tagId))
+        self.conn.commit()
+
+    def insertManyNews(self, list):
+        sql = """
+        INSERT INTO sina_news_7x24
+        (sina_id, create_time, content, url, significance)
+        VALUES(?, ?, ?, ?, ?, ?)
+        """
         self.conn.executemany(sql, list)
+        self.conn.commit()
+
+    def insertManyTags(self, tags):
+        sql = """
+        INSERT OR IGNORE INTO tag (name, is_sina_tag, sina_id) VALUES (?,?,?)
+        """
+        self.conn.executemany(sql, tags)
+        self.conn.commit()
+
+    def insertManyRelations(self, relations):
+        sql = """
+        INSERT OR IGNORE INTO news_tag (news_id, tag_id) VALUES (?,?)
+        """
+        self.conn.executemany(sql, relations)
         self.conn.commit()
 
     def updateNewsSignificance(self, id, significance):
@@ -130,10 +173,20 @@ class SinaNews7x24DB:
         )
 
         sql = f"""
-            SELECT * FROM sina_news_7x24
-            {conditionStr}
-            ORDER BY create_time {'DESC' if sort == 0 else 'ASC'}
-            LIMIT :pageSize OFFSET :pageSize*:page
+        SELECT n.id, n.create_time, n.content, n.significance, GROUP_CONCAT(t.id, ',') as tags 
+        FROM sina_news_7x24 n
+        JOIN news_tag nt 
+        ON n.id = nt.news_id
+        JOIN tag t
+        ON t.id = nt.tag_id
+        WHERE n.id IN (
+            SELECT news_id from news_tag nt
+            WHERE nt.tag_id = 1
+        )
+        {conditionStr}
+        GROUP BY n.id
+        ORDER BY create_time {'DESC' if sort == 0 else 'ASC'}
+        LIMIT :pageSize OFFSET :pageSize*:page
         """
 
         cursor = self.conn.execute(
@@ -186,20 +239,19 @@ class SinaNews7x24DB:
 
     def clear(self):
         self.conn.execute("DROP TABLE sina_news_7x24")
-        self.conn.execute(
-            "UPDATE sqlite_sequence SET seq = 0 WHERE name = 'sina_news_7x24'"
-        )
+        self.conn.execute("UPDATE sqlite_sequence SET seq = 0 WHERE name = 'sina_news_7x24'")
         self.conn.execute("DROP TABLE tag")
         self.conn.execute("UPDATE sqlite_sequence SET seq = 0 WHERE name = 'tag'")
+        self.conn.execute("DROP TABLE news_tag")
         self.conn.commit()
 
     def destroy(self):
         self.conn.close()
 
 
-# if __name__ == "__main__":
-#   db = SinaNews7x24DB()
-#   db.recoverFromJson('data/sina7x24.json')
-#   print(db.selectNewsTags())
-#   print(db.selectNews(10, 100))
-#   # print(db.newsCount())
+if __name__ == "__main__":
+    db = SinaNews7x24DB()
+    db.recoverFromJson("data/sina7x24.json")
+    print(db.selectNewsTags())
+    print(db.selectNews(10, 100))
+    # print(db.newsCount())
