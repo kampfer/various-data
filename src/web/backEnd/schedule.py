0d2bab5a7cfe27966c5e9611f684b3fc8d2678a6
liaowei <liaowei@vcg.vom>
2024-06-17 10:34:31 +0800

    update

--- src/web/backEnd/schedule.py
+++ src/web/backEnd/schedule.py
@@ -2,18 +2,32 @@ from sinaNews import crawlSinaNews, closeSinaDB
 from apscheduler.schedulers.blocking import BlockingScheduler
 import signal
 
+
 def crawlSinaJob():
     crawlSinaNews()
 
+
 def closeDB():
-    print('Closing database connection...')
+    print("Closing database connection...")
     closeSinaDB()
+
 
 def schedule_task():
     scheduler = BlockingScheduler()
     signal.signal(signal.SIGINT, signal_handler)
     try:
-        scheduler.add_job(crawlSinaJob, 'interval', hours=1, misfire_grace_time=60)
+        # 详见：https://apscheduler.readthedocs.io/en/3.x/modules/schedulers/base.html#apscheduler.schedulers.base.BaseScheduler.add_job
+        # misfire_grace_time
+        # coalesce
+        # replace_existing
+        # max_instances
+        scheduler.add_job(
+            crawlSinaJob,
+            "interval",
+            hours=1,
+            misfire_grace_time=60,
+            replace_existing=True,
+        )
         crawlSinaJob()  # 立即执行一次抓取
         scheduler.start()
         while True:
@@ -25,9 +39,10 @@ def schedule_task():
     finally:
         print("程序结束")
 
+
 def signal_handler(signal, frame):
     raise KeyboardInterrupt
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     schedule_task()
