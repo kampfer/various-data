fa685117d5a704ecf1788d3afef65ac14297959c
liaowei <liaowei@vcg.vom>
2024-07-29 17:32:38 +0800

    调试模式

--- src/web/backEnd/database.py
+++ src/web/backEnd/database.py
@@ -1,9 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///various_data_dev.db"
+SQLALCHEMY_DATABASE_URL = (
+    "sqlite:///various_data_dev.db"
+    if os.getenv("DEBUG") == "1"
+    else "sqlite:///various_data.db"
+)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
