dc54bd7af029668670142186abf0eab02ae4d9ef
liaowei <mr_liaowei@163.com>
2024-09-16 15:02:20 +0800

    把日志保存在本地

--- src/web/backEnd/app/logger.py
+++ src/web/backEnd/app/logger.py
@@ -1,15 +1,20 @@
 import logging
+import os
 
 # create logger
-logger = logging.getLogger('various-data')
+logger = logging.getLogger("various-data")
 logger.setLevel(logging.DEBUG)
 
 # create console handler and set level to debug
-ch = logging.StreamHandler()
+# ch = logging.StreamHandler()
+logFilePath = os.path.realpath(
+    os.path.join(os.path.dirname(__file__), "../../../../log/various_data.log")
+)
+ch = logging.FileHandler(logFilePath)
 ch.setLevel(logging.DEBUG)
 
 # create formatter
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s')
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s")
 
 # add formatter to ch
 ch.setFormatter(formatter)
