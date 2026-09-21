5cb956eb8ce4f801a2e743095d47b69bbeb4c2ad
liaowei <mr_liaowei@163.com>
2026-09-21 16:22:58 +0800

    chore(logger): enable dual output to console and file for development

--- src/web/backEnd/app/logger.py
+++ src/web/backEnd/app/logger.py
@@ -5,20 +5,24 @@ import os
 logger = logging.getLogger("various-data")
 logger.setLevel(logging.DEBUG)
 
-# create console handler and set level to debug
-# ch = logging.StreamHandler()
+# 同时输出到终端和日志文件，便于开发环境实时观察任务执行情况。
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.DEBUG)
+
 logFilePath = os.path.realpath(
     os.path.join(os.path.dirname(__file__), "../../../../log/various_data.log")
 )
 # 指定 utf-8 编码，避免在 Windows 上默认使用 gbk 时无法编码全角空格等字符导致 UnicodeEncodeError
-ch = logging.FileHandler(logFilePath, encoding="utf-8")
-ch.setLevel(logging.DEBUG)
+fileHandler = logging.FileHandler(logFilePath, encoding="utf-8")
+fileHandler.setLevel(logging.DEBUG)
 
 # create formatter
 formatter = logging.Formatter("%(asctime)s %(levelname)s %(name)s %(message)s")
 
-# add formatter to ch
-ch.setFormatter(formatter)
+# add formatter to handlers
+consoleHandler.setFormatter(formatter)
+fileHandler.setFormatter(formatter)
 
-# add ch to logger
-logger.addHandler(ch)
+# add handlers to logger
+logger.addHandler(consoleHandler)
+logger.addHandler(fileHandler)
